849ca065c9763a4799d27ac3cae9af9b7b7b1a09
talhagg <tgolcugezli@gmail.com>
2022-11-21 01:51:48 +0300

    Day 17/100 completed.

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Talha Gölcügezli on 16.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
